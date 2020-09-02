75fccdd89d88f2bbb4aef87e676ee1704b4042d4
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-09-02 10:53:38 -0400

    feat: adiciona configurações de testes

--- src/Buscador.php
+++ src/Buscador.php
@@ -7,27 +7,35 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class Buscador
 {
-  public function __construct(ClientInterface $httpClient, Crawler $crawler)
-  {
-    $this->httpClient = $httpClient;
-    $this->crawler = $crawler;
-  }
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+    /**
+     * @var Crawler
+     */
+    private $crawler;
 
-  public function buscar(string $url): array
-  {
-    $resposta = $this->httpClient->request('GET', $url);
-
-    $html = $resposta->getBody();
-    $this->crawler->addHtmlContent($html);
-
-    $elementosCursos = $this->crawler->filter('span.card-curso__nome');
-
-    $cursos = [];
-
-    foreach ($elementosCursos as $curso) {
-      $cursos[] = $curso->textContent;
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
     }
 
-    return $cursos;
-  }
+    public function buscar(string $url): array
+    {
+        $resposta = $this->httpClient->request('GET', $url);
+
+        $html = $resposta->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementosCursos as $elemento) {
+            $cursos[] = $elemento->textContent;
+        }
+
+        return $cursos;
+    }
 }
