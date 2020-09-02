af230d642591666a53946ca75cc5dadd6cf6a54d
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-09-02 11:19:15 -0400

    feat: adiciona script de execução

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require 'vendor/autoload.php';
@@ -6,11 +7,11 @@ use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$client = new Client(['base_uri' => 'https://www.alura.com.br']);
 $crawler = new Crawler();
 
-$buscardor = new Buscador($client, $crawler);
-$cursos = $buscardor->buscar('/cursos-online-programacao/php');
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
     echo exibeMensagem($curso);
